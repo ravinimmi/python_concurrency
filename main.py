d3901f3167408188bf0584e26cb835afe5de98de
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2023-07-23 13:12:07 +0530

    Add code for multithreaded requests

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import sys
 
 import requests
@@ -18,9 +19,17 @@ def http_requests_serial(urls, count):
             http_request_serial(url)
 
 
+@timeit
+def http_requests_mutithreading(urls, count):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(http_request_serial, url) for url in urls] * count
+        for future in concurrent.futures.as_completed(futures):
+            future.result()
+
 
 if __name__ == '__main__':
     urls = sys.argv[1:]
     print(urls)
 
-    http_requests_serial(urls, 10)
+    # http_requests_serial(urls, 20)
+    http_requests_mutithreading(urls, 100)

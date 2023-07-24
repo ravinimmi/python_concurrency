89c55d12a7197a625fda2bab6a500a685b59696b
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2023-07-24 16:09:07 +0530

    Refactor

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from decorators import timeit, timeit_async
 
 
 @timeit
-def http_request_serial(url):
+def http_request(url):
     request = requests.get(url)
     print(f"Response length: {len(request.content)}")
 
@@ -18,13 +18,13 @@ def http_request_serial(url):
 def http_requests_serial(urls, count):
     for _ in range(count):
         for url in urls:
-            http_request_serial(url)
+            http_request(url)
 
 
 @timeit
 def http_requests_mutithreading(urls, count):
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = [executor.submit(http_request_serial, url) for url in urls * count]
+        futures = [executor.submit(http_request, url) for url in urls * count]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
@@ -32,13 +32,13 @@ def http_requests_mutithreading(urls, count):
 # @timeit
 def http_requests_mutiprocessing(urls, count):
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = [executor.submit(http_request_serial, url) for url in urls * count]
+        futures = [executor.submit(http_request, url) for url in urls * count]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
 
 @timeit_async
-async def http_request_serial_coroutine(url):
+async def http_request_coroutine(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             print(f"Response length: {len(await response.text())}")
@@ -48,7 +48,7 @@ async def http_requests_asyncio_coroutine(urls, count):
     async with asyncio.TaskGroup() as tg:
         for _ in range(count):
             for url in urls:
-                tg.create_task(http_request_serial_coroutine(url))
+                tg.create_task(http_request_coroutine(url))
 
 
 @timeit
@@ -63,23 +63,3 @@ if __name__ == '__main__':
     # http_requests_mutithreading(urls, 1000)
     # http_requests_mutiprocessing(urls, 1000)
     http_requests_asyncio(urls, 1000)
-
-    """
-    Single request ~500ms
-    
-    Serial
-    count  time cpu   mem
-    100    52s   8%   0.4%
-    
-    Multiprocessing - 8
-    count  time cpu   mem
-    1000   55s  50%   3.2%
-    
-    Multithreading
-    count  time cpu   mem
-    1000   45s  50%   0.5%
-    
-    Asyncio
-    count  time cpu   mem
-    1000   4s   100%  0.5%     
-    """

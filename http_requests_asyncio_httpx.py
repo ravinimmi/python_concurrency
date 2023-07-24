8659a0d57af73ba9474666a66ab8a103829135a8
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2023-07-24 16:40:04 +0530

    Add code for asyncio with httpx

---
+++ http_requests_asyncio_httpx.py
@@ -0,0 +1,29 @@
+import asyncio
+import sys
+import httpx
+
+from decorators import timeit, timeit_async
+
+
+@timeit_async
+async def http_request_coroutine(url):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        print(f"Response length: {len(response.content)}")
+
+
+async def http_requests_asyncio_coroutine(urls, count):
+    async with asyncio.TaskGroup() as tg:
+        for _ in range(count):
+            for url in urls:
+                tg.create_task(http_request_coroutine(url))
+
+
+@timeit
+def http_requests_asyncio(urls, count):
+    asyncio.run(http_requests_asyncio_coroutine(urls, count))
+
+
+if __name__ == '__main__':
+    urls = sys.argv[1:]
+    http_requests_asyncio(urls, 1000)

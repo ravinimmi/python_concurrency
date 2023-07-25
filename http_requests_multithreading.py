f7453d07fdb3363a59f3ae037db4c880288d479f
Ravi Kumar Nimmi <nimmi.ravikumar@gmail.com>
2023-07-25 20:10:38 +0530

    Add results for different thread counts

--- http_requests_multithreading.py
+++ http_requests_multithreading.py
@@ -6,13 +6,14 @@ from decorators import timeit
 
 
 @timeit
-def http_requests_mutithreading(urls, count):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+def http_requests_mutithreading(urls, count, worker_count=None):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=worker_count) as executor:
         futures = [executor.submit(http_request, url) for url in urls * count]
         for future in concurrent.futures.as_completed(futures):
             future.result()
 
 
 if __name__ == '__main__':
-    urls = sys.argv[1:]
-    http_requests_mutithreading(urls, 1000)
+    worker_count = int(sys.argv[1]) or None
+    urls = sys.argv[2:]
+    http_requests_mutithreading(urls, 1000, worker_count)
